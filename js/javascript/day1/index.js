03f18ca2a7ab775a3f2301be1bde86b7656b1071
Ramprakash Yadav <ramprakashyadav2027@gmail.com>
2025-09-30 12:35:33 +0530

    /

--- js/javascript/day1/index.js
+++ js/javascript/day1/index.js
@@ -2,87 +2,63 @@ console.log("hello world");
 
 const element = document.getElementById("hello");
 element.style.backgroundColor = "red";
-console.log(b);
+// console.log(b); // 'b' is not defined, so this line is removed
 
-const a =function(name,age,branch) {
-   return "my name is "+name+" age "+age+" branch "+branch;
-}
+const a = function(name, age, branch) {
+    return "my name is " + name + " age " + age + " branch " + branch;
+};
 
-// console.log(info("adi",12,"cse"));
-console.log(a("adi",12,"cse"));
+console.log(a("adi", 12, "cse"));
 
+// Arrow function
+const b = (name, age, branch) => {
+     return "my name is " + name + " age " + age + " branch " + branch;
+};
+console.log(b("adi", 12, "cse"));
 
-Array function
-
-const a=(name,age,branch)=>{
-    return "my name is "+name+" age "+age+" branch "+branch;
-}
-console.log(a("adi",12,"cse"));
-
-function info(str="Hii")
-{
-    console.log("hello "+str);
-    
+function info(str = "Hii") {
+     console.log("hello " + str);
 }
 info("How are you");
 
-Iife
-
-(function(){
-    console.log("hii i am inside the function");
-    
+// IIFE
+(function() {
+     console.log("hii i am inside the function");
 })();
 
-setTimeout(function() {console.log("hiiii")},1000
-)
+setTimeout(function() {
+     console.log("hiiii");
+}, 1000);
 
-
-function lan(str)
-{
-    let data;
-    if(str=="java")
-    {
-       function javacomplier()
-       {
-        return "java compiler";
-       }
-       data=javacomplier();
-        
-    }
-     if(str=="c")
-    {
-       function Ccomplier()
-       {
-        return "c compiler";
-       }
-       data=Ccomplier();
-        
-    }
-    else
-    {
-        data="not appropraite"
-    }
-    return data;
-
+function lan(str) {
+     let data;
+     if (str == "java") {
+          function javacomplier() {
+                return "java compiler";
+          }
+          data = javacomplier();
+     } else if (str == "c") {
+          function Ccomplier() {
+                return "c compiler";
+          }
+          data = Ccomplier();
+     } else {
+          data = "not appropraite";
+     }
+     return data;
 }
 console.log(lan(" "));
-
 
 let x = 10;
 
 if (true) {
-  let x = 20;
-  console.log(x); 
+     let x = 20;
+     console.log(x);
 }
-function ram()
-{
-    let x=90;
-    console.log(x);
-    
+
+function ram() {
+     let x = 90;
+     console.log(x);
 }
 ram();
 console.log(x);
-
-
-
-
---
+++ js/javascript/jsmodule/main.js
@@ -0,0 +1,8 @@
+// // import {sum,multiply} from './logic.js'
+// const {sum,multiply} =require("./logic.js")
+// const data=sum(90,200)
+// console.log(data)
+// const mul=multiply(8,9)
+// console.log(mul);
+
+
