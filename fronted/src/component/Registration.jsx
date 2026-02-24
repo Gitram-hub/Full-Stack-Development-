de2bc173d1f3311ec20c148d7f562c3aa1c377e9
Ramprakash Yadav <ramprakashyadav2027@gmail.com>
2026-02-24 10:32:12 +0530

    /

--- fronted/src/component/Registration.jsx
+++ fronted/src/component/Registration.jsx
@@ -1,48 +1,59 @@
 import React, { useState } from 'react'
 
-function Registration({regData}) {
+function Registration() {
 
-  const[name,setName]=useState();
-  const[email,setEmail]=useState();
-  const[password,setPassword]=useState();
+  const [name,setName]=useState("")
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("")
 
-  function getData(e){
-   e.preventDefault();
-    // alert("Hii"+name+email+password);
-    const data={
-      name,
-      email,
-      password
-    }
-    regData(data);
+  async function handleSubmit(e){
+    e.preventDefault()
+
+    const data={name,email,password}
+
+    const response=await fetch('http://localhost:4002/register',{
+      method:'POST',
+      body:JSON.stringify(data),
+      headers:{'Content-Type':'application/json'}
+    })
+
+    const jsonres=await response.json()
+    alert(jsonres.msg)
   }
+
   return (
     <div>
-        <h2>Registration Form</h2>
-        <form>
-            <div class="form-group">
-    <label for="exampleInputEmail1">Name</label>
-    <input type="text" onChange={(e)=>setName(e.target.value)} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-    
-  </div>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" onChange={(e)=>setEmail(e.target.value)} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" onChange={(e)=>setPassword(e.target.value)} class="form-control" id="exampleInputPassword1" />
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" onClick={getData} class="btn btn-primary">Register</button>
-</form>
+      <h2>Registration Form</h2>
 
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" 
+            onChange={(e)=>setName(e.target.value)} 
+            className="form-control" 
+            id="name" />
+        </div>
 
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" 
+            onChange={(e)=>setEmail(e.target.value)} 
+            className="form-control" 
+            id="email" />
+        </div>
 
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" 
+            onChange={(e)=>setPassword(e.target.value)} 
+            className="form-control" 
+            id="password" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Register
+        </button>
+      </form>
     </div>
   )
 }
